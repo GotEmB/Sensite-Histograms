e0040cfeb752c310fe687e5d9b8d8bfd83b8bb9f
Gautham Badhrinathan <b.gautham@gmail.com>
2014-05-08 19:32:41 -0700

    Initial Commit

---
+++ associations_frequency.r
@@ -0,0 +1,22 @@
+#!/usr/bin/Rscript
+
+library(methods)
+library(rJava)
+library(RMongo)
+library(rjson)
+
+args <- commandArgs(TRUE)
+phenomenon <- args[1]
+
+dbm <- mongoDbConnect("sensite", host="ds033699.mongolab.com", port=33699)
+auth <- dbAuthenticate(dbm, "sensite130", "sensite130")
+
+associationRecord <- dbGetQuery(dbm, "associations", sprintf("{phenomenon: \"%s\"}", phenomenon))[1, ]
+sensors <- do.call(rbind, fromJSON(associationRecord[, "association"]))
+
+
+X11()
+par(las=2)
+par(mar=c(10, 5, 5, 2))
+with(mtcars, barplot(unlist(sensors[, 2]), names.arg=unlist(sensors[, 1]), main=sprintf("Phenomenon: %s", phenomenon), ylab="Count"))
+ignore <- locator(1)
